b73a52561e28244d2000986792ab3498166c577c
Leslie <lesliekanderson@fastmail.com>
2024-03-04 15:54:26 -0500

    updating yaml file

--- common/pg_methods.go
+++ common/pg_methods.go
@@ -15,7 +15,7 @@ var (
 )
 
 const (
-	host     = "msds432-cbi-phase3:us-central1:cbipostgres"
+	host     = "/cloudsql/msds432-cbi-phase3:us-central1:cbipostgres"
 	port     = 5432
 	user     = "postgres"
 	password = "root"
